e7af9f8864c805ff317e852bf5d326aee329c234
tangzhuoqiang <www.916635743@qq.com>
2022-02-27 20:53:20 +0800

    第一次提交

---
+++ src/main/java/com/bdqn/git.java
@@ -0,0 +1,14 @@
+package com.bdqn;
+
+public class git {
+    public static void main(String[] args) {
+        System.out.println("hello word git");
+        System.out.println("hello word git");
+        System.out.println("hello word git");
+        System.out.println("hello word git");
+    }
+    public void test(){
+        int i=0;
+        i+=10;
+    }
+}
